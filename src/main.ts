c8c8c318eccf0146c8766f7ab36d36347f1142d0
yhj0121 <boys1541@naver.com>
2024-06-02 19:16:09 +0900

    feat:able cors

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
-  // if (process.env.NODE_ENV !== 'production') {
-  //   app.enableCors({ origin: 'http://localhost:3000', credentials: true });
-  // }
-  // app.useGlobalPipes(new ValidationPipe());
+  if (process.env.NODE_ENV !== 'production') {
+    app.enableCors({ origin: '*', credentials: true });
+  }
+
   const port = Number.parseInt(configService.get(`REST_API_PORT`));
   console.log(port);
 
